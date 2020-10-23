1683d7a72d6a2a07c0e1a14213d66ea4ac65646f
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2020-10-23 20:01:38 +0600

    create endpoint for api root

--- tutorial/rip_auth/urls.py
+++ tutorial/rip_auth/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from rip_auth.views import UserList, UserDetail, GroupList, GroupDetail
+from rip_auth.views import api_root, UserList, UserDetail, GroupList, GroupDetail
 from rest_framework.authtoken import views
 
 urlpatterns = [
-    path('token-auth/', views.obtain_auth_token),
-    path('users/', UserList.as_view()),
+    path('',api_root),
+    path('users/', UserList.as_view(), name='user-list'),
     path('users/<int:pk>/', UserDetail.as_view()),
     path('groups/', GroupList.as_view()),
     path('groups/<int:pk>/', GroupDetail.as_view()),
+    path('token-auth/', views.obtain_auth_token),
 ]
--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -3,8 +3,8 @@ from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-details'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
